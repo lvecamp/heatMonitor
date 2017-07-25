d943e3084e688730ab4541e44799423a9649d2d8
Lars Vestergaard <lav@gomspace.com>
2017-07-25 15:30:29 +0200

    test

--- heatMonitor.ino
+++ heatMonitor.ino
@@ -3,22 +3,29 @@
 #include <ESP8266mDNS.h>
 #include <WiFiUdp.h>
 #include <ArduinoOTA.h>
-#include <ModbusMaster.h>
 #include "RemoteDebug.h"
 
 // Network credentials
 const char* ssid = "vestergaard";
 const char* password = "dannevirke";
 
-// instantiate ModbusMaster object
-ModbusMaster node;
+uint32_t mLastTime = 0;
+uint32_t mTimeSeconds = 0;
+int number = 2;
+char str[] = "Hello World!";
+int indexData = 0;
+char data[10];
+
+// instantiate RemoteDebug object
+RemoteDebug Debug;
 
 void setup() {
   // use Serial (port 0); initialize Modbus communication baud rate
   Serial.begin(38400);
+  Serial.setTimeout(100);
 
   // communicate with Modbus slave ID 1 over Serial (port 0)
-  node.begin(1, Serial);
+ // node.begin(1, Serial);
 
   WiFi.mode(WIFI_STA);
   WiFi.begin(ssid, password);
@@ -27,49 +34,45 @@ void setup() {
     ESP.restart();
   }
 
+  // Initialize the telnet server of RemoteDebug
+  Debug.begin("Telnet_HostName"); // Initiaze the telnet server
 
-  // Port defaults to 8266
-  // ArduinoOTA.setPort(8266);
-
-  // Hostname defaults to esp8266-[ChipID]
-  // ArduinoOTA.setHostname("myesp8266");
-
-  // No authentication by default
-  // ArduinoOTA.setPassword((const char *)"123");
-
-  ArduinoOTA.onStart([]() {
-  });
-  ArduinoOTA.onEnd([]() {
-  });
-  ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
-  });
-  ArduinoOTA.onError([](ota_error_t error) {
-  });
   ArduinoOTA.begin();
 }
 
 void loop() {
   ArduinoOTA.handle();
   
-//  Serial.print("Looping");
   static uint32_t i;
-  uint8_t j, result;
-  uint16_t data[6];
+  String incomingString;
   
-  i++;
+  byte message[] = {0x43, 0x01, 0x00, 0x03, 0x02 };
   
-  // set word 0 of TX buffer to least-significant word of counter (bits 15..0)
-  node.setTransmitBuffer(0, lowWord(i));
-  
-  // set word 1 of TX buffer to most-significant word of counter (bits 31..16)
-  node.setTransmitBuffer(1, highWord(i));
-  
-  // slave: write TX buffer to (2) 16-bit registers starting at register 0
-  result = node.writeMultipleRegisters(0, 2);
-  
-  // slave: read (6) 16-bit registers starting at register 2 to RX buffer
-  result = node.readHoldingRegisters(2, 6);
+  if ((millis() - mLastTime) >= 1000) {
 
-  delay(2000);
+    // Time
 
+    mLastTime = millis();
+
+    mTimeSeconds++;
+    i++;
+    
+  
+    Serial.write(message, sizeof(message));
+
+    while (Serial.available() > 0) {
+      // read the incoming byte:
+//      data[indexData]= Serial.read();
+//      indexData++;
+      incomingString = Serial.readString();
+    }
+ 
+    Debug.println(incomingString);
+     
+  }
+    
+
+  Debug.handle();
+  yield();
+  
 }
